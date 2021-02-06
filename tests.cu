27be32fad03e7003466f4f71acb83d76f5976309
Galen Guyer <galen@galenguyer.com>
2021-02-06 12:23:18 -0500

    fix hash function, all tests pass!

--- tests.cu
+++ tests.cu
@@ -7,9 +7,9 @@ char* hash(const char* h_str) {
     char* d_str;
     unsigned char* h_res = (unsigned char*)malloc(sizeof(unsigned char)*(32 + 1));
     unsigned char* d_res;
-    cudaMalloc((void**)&d_str, sizeof(char));
-    cudaMalloc((void**)&d_res, sizeof(char)* 32);
-    cudaMemcpy(d_str, h_str, sizeof(char), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&d_str, sizeof(char) * strlen(h_str));
+    cudaMalloc((void**)&d_res, sizeof(char) * 32);
+    cudaMemcpy(d_str, h_str, sizeof(char) * strlen(h_str), cudaMemcpyHostToDevice);
 
     md5<<<1, 1>>>(d_str, (uint32_t)strlen(h_str), d_res);
 
